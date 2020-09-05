d92145cfffd70bb26659d22af315de3880990c50
Daniel G <replayattack@pm.me>
2020-09-04 22:53:18 -0500

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
-  theme: {},
+  purge: {
+    mode: 'layers',
+    layers: ['base', 'components', 'utilities'],
+    content: ['./src/**.tsx'],
+  },
+  theme: {
+    extend: {},
+  },
   variants: {},
-  plugins: [require('@tailwindcss/ui')],
+  plugins: ['./components/**/*.tsx', './pages/**/*.tsx'],
   future: {
+    darkModeVariant: true,
+    purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
 }
